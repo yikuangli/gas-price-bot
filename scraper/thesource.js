d454229c1d5b5d6d4c966b90b894ee4b417f312a
Yiqian Li <yiqianliwinnipeg@gmail.com>
2024-09-25 22:05:30 -0400

    add Sony check

--- scraper/thesource.js
+++ scraper/thesource.js
@@ -3,6 +3,8 @@ const chromium = playwright.chromium;
 
 const tplinkweburl = "https://www.thesource.ca/en-ca/brands/tp-link/c/tp-link?categoryCode=tp-link&view=grid&page=0&sort=price-desc&cleanPageNum=true&q=%3Aprice-desc"
 const boseurl = "https://www.thesource.ca/en-ca/brands/bose/c/bose?categoryCode=bose&view=grid&page=0&sort=relevance"
+const sonyurl = "https://www.thesource.ca/en-ca/audio-headphones/headphones/all-headphones/c/scc-6-1-9?categoryCode=scc-6-1-9&view=grid&sort=relevance&cleanPageNum=true&q=%3Arelevance%3AFT-Headphones-Type%3AOver-Ear%3Abrand%3ASONY%3Abrand%3ASONY&page=0&show=All"
+
 const thesource = async () => {
    
     let productListTag = "div.product-info"
@@ -23,17 +25,22 @@ const thesource = async () => {
     const browser = await chromium.launch();
     const page = await browser.newPage();
     await page.goto(tplinkweburl);
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(5000);
     let cards = await page.$$(productListTag)
     const tplinkcardDetails = await Promise.all(cards.map(card => extractDetailsFromCard(card)))
     
    
     await page.goto(boseurl);
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(5000);
     let bosecards = await page.$$(productListTag)
     const bosecardDetails = await Promise.all(bosecards.map(card => extractDetailsFromCard(card)))  
     if (browser) await browser.close();
-    let total = [...tplinkcardDetails, ...bosecardDetails]
+
+    await page.goto(sonyurl);
+    await page.waitForTimeout(5000);
+    let sonycards = await page.$$(productListTag)
+    const sonycardDetails = await Promise.all(sonycards.map(card => extractDetailsFromCard(card)))  
+    let total = [...tplinkcardDetails, ...bosecardDetails, ...sonycardDetails]
     total = total.filter(item => item.price.includes(".96"))
     return total
     } catch (e) {
